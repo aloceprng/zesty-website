4c95c6d793f13d9b6c21162d1f2f16c3d682f9d3
pengpei1 <alice.peng@mail.utoronto.ca>
2026-02-22 23:21:50 -0500

    Revert gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/zesty-website/' : '/',
+  base: '/',
   plugins: [react()],
 }))
