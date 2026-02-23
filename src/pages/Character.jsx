39fb02520850dbbc61ab6321d4dcc59ae48b381c
pengpei1 <alice.peng@mail.utoronto.ca>
2026-02-22 22:33:57 -0500

    Character page

--- src/pages/Character.jsx
+++ src/pages/Character.jsx
@@ -1,13 +1,13 @@
 import Model from "../components/Model";
 import { Canvas } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import RepelText from "../components/RepelText";
 
 export default function Character() {
     const { characterName } = useParams();
     const [character, setCharacter] = useState(null);
-    console.log(characterName);
 
     useEffect(() => {
         fetch('/characters.json')
@@ -18,8 +18,10 @@ export default function Character() {
         })
     }, [characterName])
 
+    if (!character) return <div>Loading...</div>;
+
     return (
-        <div className="character-page">
+        <div className="character-page page">
             <div className="character-container">
                 <Canvas camera={{ position: [0, 0.5, 2], fov: 80 }}>
                     <ambientLight intensity={0.2} color="0xfdfff4"/>
@@ -27,16 +29,19 @@ export default function Character() {
                     <OrbitControls 
                         autoRotate={true}
                         autoRotateSpeed={8}
-                    />
+                        />
                     <Environment preset="city" />
-                    <Model name={character?.name} state="default" position={[0, -1.25, 0]}/>
+                    <Model name={character.name} state="default" position={[0, -1.25, 0]}/>
                 </Canvas>
             </div>
             <div className="text-container">
-                <p> {character?.description}</p>
-                <p> favourite things: {character?.favourite}</p>
+                <Link to={"/characters"}>back to characters</Link>
+                <RepelText text={`${character.name}`}/>
+                <p> {character.description}</p>
+                <span> favourite things: {character.favourite}</span>
+                <p id="statement"> "{character.statement}" </p>
             </div>
-            
+
         </div>
     )
 }
